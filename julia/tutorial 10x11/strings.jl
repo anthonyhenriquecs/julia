6e65cbfd0ae04c5c68f230bf3bcd900101a366c5
Henrique Anthony <107704669+anthonyhenriquecs@users.noreply.github.com>
2024-06-19 21:10:20 -0300

    Add files via upload

---
+++ julia/tutorial 10x11/strings.jl
@@ -0,0 +1,10 @@
+typeof("a") # pode ser varios caracteres
+typeof('a') # so pode ter um unico caractere
+
+println("1\n2\n3") #Para criar novas linhas no texto
+
+println("1\t2\t3")#Para dar tab no texto
+
+n1 = "Ola, "
+n2 = "Mundo"
+texto = n1*n2#para concatenar
---
+++ julia/tutorial arrays/array.jl
@@ -0,0 +1,19 @@
+# Em julia uma matriz unidimenssional é chamada de vetor
+#1 coluna
+
+# []
+#insira os dados separados por ,
+vetor = [1,2, 3]
+println(vetor)
+
+#Os dados individuais são chamados de "elementos"
+#=Para acessar um elemento individal em uma matriz use []dps do nome
+entre os parenteses coloque o numero do indice=#
+##name[index#]
+
+#Pode alterar o valor
+vetor[2]= 10
+print(vetor)
+
+#Somar valores dentrodo vetor
+sum(vetor)
